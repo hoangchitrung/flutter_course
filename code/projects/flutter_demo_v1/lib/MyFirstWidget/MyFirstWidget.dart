b8ce82e6dc310c4dae38b03cf59881ad19dc4671
hoangchitrung <trunghoang1124@gmail.com>
2025-11-28 09:03:28 +0700

    runned

---
+++ code/projects/flutter_demo_v1/lib/MyFirstWidget/MyFirstWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Myfirstwidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Text(
+          "Hello World", style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
