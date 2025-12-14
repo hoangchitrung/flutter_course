987ae664432335720619b0913fc93afb948a196f
hoangchitrung <trunghoang1124@gmail.com>
2025-12-14 16:54:39 +0700

    update: now can select category and it will return the original of books list.

--- code/projects/flutter_demo_v1/lib/screens/BookListDemo.dart
+++ code/projects/flutter_demo_v1/lib/screens/BookListDemo.dart
@@ -15,9 +15,16 @@ class BookListDemo extends StatefulWidget {
 
 class _BookListDemoState extends State<BookListDemo> {
   List<Book> books = [];
-  List<Book> filteredBook = [];
+  List<Book> searchBook = [];
+  List<Book> result = [];
 
+  final categories = {'Basic': false, 'Advanced': false};
+
+  String filterQuery = "";
+  String query = "";
+  late bool checkboxStatus;
   String dataPath = "assets/data/books.json";
+
   Future<void> loadBooks() async {
     try {
       final res = await rootBundle.loadString(dataPath);
@@ -28,22 +35,56 @@ class _BookListDemoState extends State<BookListDemo> {
           return Book.fromJson(item);
         }).toList();
       });
-      filteredBook = books;
+      searchBook = books;
     } catch (e) {
       print("Error while loading: $e");
     }
   }
 
-  void filterBook(String query) {
-    if (query.isEmpty) {
-      filteredBook = books;
-    } else {
-      filteredBook = books
-          .where(
-            (book) => book.name.toLowerCase().contains(query.toLowerCase()),
-          )
+  void applyFilters() {
+    setState(() {
+      // select categories
+      final selectedCategories = categories.entries
+          .where((e) => e.value == true) // take entry value as true
+          .map((e) => e.key) // extract category name
           .toList();
-    }
+
+      // search book by name
+      if (query.isNotEmpty) {
+        searchBook = books
+            .where(
+              (book) => book.name.toLowerCase().contains(query.toLowerCase()),
+            )
+            .toList();
+      } else {
+        searchBook = books;
+      }
+      // filters book categories
+      // if there is no search
+      if (filterQuery.isNotEmpty) {
+        result = books
+            .where(
+              (book) => book.category.toLowerCase().contains(
+                filterQuery.toLowerCase(),
+              ),
+            )
+            .toList();
+        searchBook = result;
+      }
+      // When uncheck it will return the original list of books
+      if (checkboxStatus == false) {
+        searchBook = books;
+      }
+      // if (selectedCategories.isNotEmpty) {
+      //   result = searchBook
+      //       .where(
+      //         (book) => book.category.toLowerCase().contains(
+      //           selectedCategories.toString().toLowerCase(),
+      //         ),
+      //       )
+      //       .toList();
+      // }
+    });
   }
 
   @override
@@ -61,6 +102,7 @@ class _BookListDemoState extends State<BookListDemo> {
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20),
             child: TextField(
+              // Search bar
               decoration: InputDecoration(
                 hintText: "Search...",
                 prefixIcon: Icon(Icons.search),
@@ -69,61 +111,106 @@ class _BookListDemoState extends State<BookListDemo> {
                 ),
               ),
               onChanged: (value) {
-                setState(() {
-                  filterBook(value);
-                });
+                query = value;
+                applyFilters();
               },
             ),
           ),
           SizedBox(height: 10),
-          Expanded(
-            child: ListView.builder(
-              itemCount: filteredBook.length,
-              itemBuilder: (context, index) {
-                final book = filteredBook[index];
-                return Padding(
-                  padding: EdgeInsets.all(5),
-                  child: Card(
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Image.asset(book.image, width: 50, height: 50),
-                          Column(
-                            children: [
-                              Text(
-                                book.name,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                ),
-                              ),
-                              Text(
-                                book.price.toString(),
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Text(
-                            book.category,
-                            style: TextStyle(fontSize: 20, color: Colors.black),
-                          ),
-                          book.active
-                              ? Icon(Icons.check, color: Colors.green)
-                              : Icon(Icons.cancel, color: Colors.red),
-                        ],
-                      ),
+
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Text("Filters", style: TextStyle(fontSize: 20)),
+            ),
+          ),
+
+          Row(
+            children: [
+              ...categories.entries.map((e) {
+                return Row(
+                  children: [
+                    Checkbox(
+                      value: e.value,
+                      onChanged: (value) {
+                        setState(() {
+                          categories[e.key] = value!;
+                          filterQuery = e.key;
+                          checkboxStatus = value;
+                          applyFilters();
+                        });
+                      },
                     ),
-                  ),
+                    Text(e.key),
+                  ],
                 );
+              }).toList(),
+            ],
+          ),
+
+          SizedBox(height: 10),
+
+          Expanded(
+            // Showing book card
+            child: ListView.builder(
+              itemCount: searchBook.length,
+              itemBuilder: (context, index) {
+                final book = searchBook[index];
+                return BookCard(book: book);
               },
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+// Create a class for designing the book card
+class BookCard extends StatelessWidget {
+  final Book book;
+
+  BookCard({required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(book.image, width: 50, height: 50),
+              Column(
+                children: [
+                  Text(
+                    book.name,
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        book.price.toString(),
+                        style: TextStyle(fontSize: 15, color: Colors.black),
+                      ),
+                      Icon(Icons.currency_pound),
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                book.category,
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+              book.active
+                  ? Icon(Icons.check, color: Colors.green)
+                  : Icon(Icons.cancel, color: Colors.red),
+            ],
+          ),
+        ),
       ),
     );
   }
